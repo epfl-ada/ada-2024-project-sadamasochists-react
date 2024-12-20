70ea27715b892877b2ace26662965d2a39251af1
Not Alex <alex@totallynotmyemail.com>
2024-12-20 13:39:25 +0100

    pages

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Disable ESLint checks during the build
   },
-  /* Add other config options here if needed */
+  output: "export", // Ensure the app is statically exported
+  basePath: "/ada-2024-project-sadamasochists-react", // GitHub Pages base path
+  trailingSlash: true, // Add trailing slashes to routes for static deployment
 };
 
 export default nextConfig;
